79c9d4c8e04b3072ccce73fb909d1b5dee622b96
odinokov <12473191+odinokov@users.noreply.github.com>
2018-05-26 23:23:28 +0800

    Create CpGs_in_FASTA.R

---
+++ CpGs_in_FASTA.R
@@ -0,0 +1,21 @@
+##################################################
+## Project: CpG islands in FASTA files
+## Script purpose:  Build a table with the Nucleotide Ref and the Number of CpGs in mtDNAs
+##                  This script assumes that 
+##                  the actual sequences of each mtDNA are downloaded via https://github.com/odinokov/MitoAge2FASTA
+##                  and processed via Perl script to predict the number of CpG islands (CGIs) http://bioinfo2.ugr.es/CpGcluster/
+##################################################
+rm(list = ls())
+setwd("D:/Dropbox/MitoAge/fasta")
+dataFiles <- lapply(Sys.glob("*CpGcluster.txt-log.txt"), read.csv)
+how_many_records <- length(dataFiles)
+CpGs_table <- data.frame(Nucleotide_Ref = character(), Number_of_CpGs_in_sequence = character())
+for(i in 1:how_many_records)
+  {
+  x <- names(dataFiles[[i]])
+  ref_ID <- substr(x, nchar(x)-10, nchar(x))
+  CpGs <- toString(dataFiles[[i]][[1]][4])
+  CpGs <- substring(CpGs, 29)
+  CpGs_table <- rbind(CpGs_table, data.frame(Nucleotide_Ref = ref_ID, Number_of_CpGs_in_sequence = CpGs))
+  }
+write.csv(CpGs_table, file="all_CpGs_from_fasta.csv")
