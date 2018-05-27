b113367a518301b1013b057f29657f4be304a758
odinokov <12473191+odinokov@users.noreply.github.com>
2018-05-27 09:38:19 +0800

    Update CpGs_in_FASTA.R

--- CpGs_in_FASTA.R
+++ CpGs_in_FASTA.R
@@ -1,24 +1,28 @@
 ##################################################
 ## Project: CpG islands in FASTA files
 ## Script purpose:  Build a table with the Nucleotide Ref and the Number of CpGs in mtDNAs
-##                  This script assumes that 
-##                  the actual sequences of each mtDNA are downloaded via https://github.com/odinokov/MitoAge2FASTA
-##                  and processed via Perl script to predict the number of CpG islands (CGIs) http://bioinfo2.ugr.es/CpGcluster/
+##                  R script to get the actual sequences of each mtDNA https://github.com/odinokov/MitoAge2FASTA
+##                  Perl script to predict the number of CpG islands (CGIs) http://bioinfo2.ugr.es/CpGcluster/
+##                  system('perl CpGcluster.pl ./fasta/ 50 0.00005')
 ##################################################
 rm(list = ls())
 setwd("D:/Dropbox/MitoAge/fasta")
+# read CpGcluster results
 dataFiles <- lapply(Sys.glob("*CpGcluster.txt-log.txt"), read.csv)
 how_many_records <- length(dataFiles)
-CpGs_table <- data.frame(Nucleotide_Ref = character(), Number_of_CpGs_in_sequence = character())
+CpGs_table <- data.frame(Nucleotide_Ref = character(), Number_of_CpGs_in_sequence = numeric(), Sequence_Length = integer(), CpG_ratio = numeric())
 for(i in 1:how_many_records)
-  {
+{
   x <- names(dataFiles[[i]])
   ref_ID <- substr(x, nchar(x)-10, nchar(x))
   # get the number of CpGs
   CpGs <- toString(dataFiles[[i]][[1]][4])
-  # get a FASTA length
-  # toString(dataFiles[[i]][[1]][1:1])
   CpGs <- substring(CpGs, 29)
-  CpGs_table <- rbind(CpGs_table, data.frame(Nucleotide_Ref = ref_ID, Number_of_CpGs_in_sequence = CpGs))
-  }
-write.csv(CpGs_table, file="all_CpGs_from_fasta.csv")
+  # get the sequence length
+  sequence_length <- toString(dataFiles[[i]][[1]][1:1])
+  sequence_length <- substring(sequence_length, 9)
+  # get the ratio CpGs/sequence length
+  ratio <- 2 * as.numeric(CpGs) / as.numeric(sequence_length)
+  CpGs_table <- rbind(CpGs_table, data.frame(Nucleotide_Ref = ref_ID, Number_of_CpGs_in_sequence = CpGs, Sequence_Length = sequence_length, CpG_ratio = ratio))
+}
+write.csv(CpGs_table, file="all_CpG_data_from_fasta.csv")
